65115992a86fbc69a73a86c65fc63c4414836e05
nidotim <nidotim@gmail.com>
2013-10-17 01:49:21 +0800

    Third

--- microblog/app.js
+++ microblog/app.js
@@ -10,19 +10,72 @@ var http = require('http');
 var path = require('path');
 var util = require('util');
 
+var MongoStore = require('connect-mongo')(express);
+var settings = require('../settings');
+
+var mongodb = require('mongodb');
+
+var mongodbServer = new mongodb.Server('localhost', 27017, { auto_reconnect: true, poolSize: 10 });
+var db = new mongodb.Db('mydb', mongodbServer);
+
+/* open db */
+db.open(function() {
+    /* Select 'contact' collection */
+    db.collection('contact', function(err, collection) {
+        /* Insert a data */
+/*        
+        collection.insert({
+            name: 'Fred Chien',
+            email: 'cfsghost@gmail.com',
+            tel: [
+                '0926xxx5xx',
+                '0912xx11xx'
+            ]
+        }, function(err, data) {
+            if (data) {
+                console.log('Successfully Insert');
+            } else {
+                console.log('Failed to Insert');
+            }
+        });
+*/
+        /* Querying */
+        collection.find({ name: 'Fred Chien' }, function(err, data) {
+            /* Found this People */
+            if (data) {
+                console.log('Name: ' + data.name + ', email: ' + data.email);                
+            } else {
+                console.log('Cannot found');
+            }
+        });
+    });
+});
 
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+//app.configure(function(){
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'ejs');
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+		
+	app.use(express.cookieParser());
+/*
+	app.use(express.session({
+		secret: settings.cookieSecret,
+		store: new MongoStore({
+			db: settings.db
+		})
+	}));
+*/
+	app.use(app.router);
+	//app.use(express.router(routes));
+	app.use(express.static(path.join(__dirname, 'public')));
+//});
 
 // development only
 if ('development' == app.get('env')) {
@@ -33,7 +86,7 @@ app.get('/', routes.index);
 app.get('/users', routes.user);
 app.get('/post', routes.post);
 app.get('/reg', routes.reg);
-app.post('reg', routes.doReg);
+app.post('/reg', routes.doReg);
 app.get('/login', routes.login);
 app.post('/login', routes.doLogin);
 app.get('/logout', routes.logout);
--- microblog/routes/index.js
+++ microblog/routes/index.js
@@ -25,7 +25,14 @@ exports.reg = function(req, res){
 
 
 exports.doReg = function(req, res){
-
+	console.log(req.body['password-repeat']);
+	console.log(req.body['password']);
+	if(req.body['password-repeat'] != req.body['password']){
+		//req.flash('error', '兩次輸入的密碼不一致');
+		req.flash('error', 'not same');
+		return res.redirect('/reg');
+	}
+	
 };
 
 
@@ -47,3 +54,18 @@ exports.hello = function(req, res){
 	res.send('The time is ' + new Date().toString());
 };
 
+
+/*
+module.exports = function(app){
+	app.get('/', function(req, res){
+		res.render('index', {title:'首頁'});
+	});
+
+	app.get('/reg', function(req, res){
+		res.render('reg', {
+			title: '使用者註冊'
+		});
+	});
+};
+
+*/
